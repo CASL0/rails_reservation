19b1d1dc0abe30855cfaddf0d6582f492c74b473
CASL0 <gin.0046.822@gmail.com>
2023-12-03 22:14:29 +0900

    feat: 会議室のバリデーション

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,2 +1,7 @@
 class Room < ApplicationRecord
+  validates :name, :place, :number, presence: true
+  validates :name, length: { maximum: 30 }
+  validates :place, inclusion: { in: ["東京", "大阪", "福岡", "札幌", "仙台", "名古屋", "金沢"] }
+  validates :number, numericality: { greater_than_or_equal_to: 5, less_than_or_equal_to: 30 }
+  validates :name, format: { with: /\A.+#\d{2}\z/ }
 end
