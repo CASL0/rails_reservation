aa3ecb9c6d211fa2c1c2f6234312455db72138ee
CASL0 <gin.0046.822@gmail.com>
2023-12-04 22:41:29 +0900

    fix: 前後7日間のデータ取得用のスコープ

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,2 +1,7 @@
 class Entry < ApplicationRecord
+  scope :least_entries,
+        lambda { |middle_day|
+          where('reserved_date <= ?', middle_day.to_date + 7.days)
+            .and(where('reserved_date >= ?', middle_day.to_date - 7.days))
+        }
 end
