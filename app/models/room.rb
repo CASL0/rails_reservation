445afb84e92bac592c1e69e805dd171203fed13c
CASL0 <gin.0046.822@gmail.com>
2023-12-04 21:29:52 +0900

    feat: 収容人数のバリデーションエラー

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,7 +1,10 @@
 class Room < ApplicationRecord
   validates :name, :place, :number, presence: true
   validates :name, length: { maximum: 30 }
-  validates :place, inclusion: { in: ["東京", "大阪", "福岡", "札幌", "仙台", "名古屋", "金沢"] }
+  validates :place, inclusion: { in: %w[東京 大阪 福岡 札幌 仙台 名古屋 金沢] }
   validates :number, numericality: { greater_than_or_equal_to: 5, less_than_or_equal_to: 30 }
-  validates :name, format: { with: /\A.+#\d{2}\z/, message: "会議室名が正しくありません" }
+  validates :name, format: { with: /\A.+#\d{2}\z/, message: '会議室名が正しくありません' }
+  validates_each :number do |record, attr, value|
+    record.errors.add(attr, '収容人数は５の倍数で指定してください') unless (value % 5).zero?
+  end
 end
